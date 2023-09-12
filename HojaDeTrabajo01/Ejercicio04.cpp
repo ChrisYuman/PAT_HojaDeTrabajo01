c91b3dca841cfd9fe750b7bcf5c05ae8c5db1642
Christopher Yuman <chrisyumanv@gmail.com>
2023-09-12 02:26:08 -0600

    Ejercicio 4, resuelto

--- HojaDeTrabajo01/Ejercicio04.cpp
+++ HojaDeTrabajo01/Ejercicio04.cpp
@@ -1,6 +1,23 @@
 #include "Ejercicio04.h"
 
+void generateParentheses(vector<string>& resultado, string aa, int abrirparen, int cerrarparen, int maximo) {
+    if (aa.length() == maximo * 2) {
+        resultado.push_back(aa);
+        return;
+    }
+
+    if (abrirparen < maximo) {
+        generateParentheses(resultado, aa + "(", abrirparen + 1, cerrarparen, maximo);
+    }
+
+    if (cerrarparen < abrirparen) {
+        generateParentheses(resultado, aa + ")", abrirparen, cerrarparen + 1, maximo);
+    }
+}
+
 vector<string> Ejercicio04::generateParenthesis(int n)
 {
-    return vector<string>();
+    vector<string> resultado;
+    generateParentheses(resultado, "", 0, 0, n);
+    return resultado;
 }
