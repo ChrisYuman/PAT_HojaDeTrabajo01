8084f81211723e96e6cd13ab5ecb565fe5e3e1e7
Christopher Yuman <chrisyumanv@gmail.com>
2023-09-12 01:33:16 -0600

    Ejercicio 2, error correcion

--- HojaDeTrabajo01/Ejercicio02.cpp
+++ HojaDeTrabajo01/Ejercicio02.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 bool Ejercicio02::isPalindrome(string& s)
 {
-    return false;
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
 }
 
